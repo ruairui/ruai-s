10ab6dbbdca3d0b31ca7f99fce7495f790060c27
RuaiRUI <14750194+ruairui@user.noreply.gitee.com>
2024-07-31 22:37:55 +0800

    第一次提交

---
+++ my first.cpp
@@ -0,0 +1,11 @@
+//dispays a message
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "come up and C++ me some time.";
+    cout << endl;
+    cout << "You won't regret it!" << endl;
+    cin.get();
+    return 0;
+}
---
+++ myfirst.cpp
@@ -0,0 +1,11 @@
+//displays a message
+#include <iostream>
+int main()
+{
+    using namespace std;
+    cout << "come up and C++ me some time.";
+    cout << end1;
+    cout << "You won't regret it!" << end1;
+    //cin.get();
+return 0;
+}
